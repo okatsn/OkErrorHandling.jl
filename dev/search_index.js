be7fa4c89a0b87e332d5b645a2594c2a5c372b09
Documenter.jl <documenter@juliadocs.github.io>
2025-11-03 08:05:25 +0000

    build based on d88cafe

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#OkErrorHandling","page":"Home","title":"OkErrorHandling","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for OkErrorHandling.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
